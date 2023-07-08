5a2f6a53c13017c7595d0d3828f5860662759204
Zaviar Brown <brown.zaviar@gmail.com>
2023-07-08 15:16:14 -0700

    Navbar & Signin refactor

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -1,5 +1,9 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 /* eslint-disable @next/next/no-img-element */
 const Navbar = () => {
+    const { data: session } = useSession();
+
     return (
         <nav
             className="fixed left-0 right-0 top-0 z-20 flex items-center 
@@ -8,21 +12,22 @@ const Navbar = () => {
             <div className="text-2xl font-semibold text-white">
                 3841 Biscayne Beach
             </div>
-            <ul className="flex space-x-4">
-                <li
-                    className="flex rounded-3xl border px-3 py-1 transition-all 
-                    duration-200 hover:scale-105"
-                >
-                    <img
-                        src="/tempPhotos/island.png"
-                        alt="Island icon by icons8.com"
-                        className="mr-1 mt-0.5 h-6"
-                    />
-                    <a href="#" className="text-xl text-white">
-                        Sign In
-                    </a>
-                </li>
-            </ul>
+            <p className="text-center text-2xl text-white">
+                {session && <span>Hello {session.user?.name}!</span>}
+            </p>
+
+            <button
+                onClick={() => (session ? void signOut() : void signIn())}
+                className="flex rounded-3xl border px-3 py-1 text-xl 
+                    text-white transition-all duration-200 hover:scale-105"
+            >
+                <img
+                    src="/tempPhotos/island.png"
+                    alt="Island icon by icons8.com"
+                    className="mr-1 mt-0.5 h-6"
+                />
+                {session ? "Sign out" : "Sign in"}
+            </button>
         </nav>
     );
 };
