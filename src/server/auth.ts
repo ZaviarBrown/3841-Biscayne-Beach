f42a9503af7b71359291929b033c2cdffda6bf4d
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-04 22:46:17 -0700

    Custom NextAuth signIn for admin users

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -10,8 +10,7 @@ import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 
 /**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
+ * Module augmentation for `next-auth` types.
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
@@ -19,15 +18,13 @@ declare module "next-auth" {
     interface Session extends DefaultSession {
         user: {
             id: string;
-            // ...other properties
-            // role: UserRole;
+            role: string;
         } & DefaultSession["user"];
     }
 
-    // interface User {
-    //   // ...other properties
-    //   // role: UserRole;
-    // }
+    interface Profile {
+        email_verified?: boolean;
+    }
 }
 
 /**
@@ -37,6 +34,71 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
     callbacks: {
+        async signIn({ user, account, profile }) {
+            if (!user.email || !account || !profile) return false;
+
+            const userCheck = await prisma.user.findUnique({
+                where: { email: user.email },
+                include: {
+                    accounts: true,
+                },
+            });
+
+           
+
+            if (!userCheck) {
+
+                await prisma.user.create({
+                    data: {
+                        name: user.name,
+                        email: user.email,
+                        emailVerified: profile.email_verified || false,
+                        image: user.image,
+                        accounts: {
+                            create: {
+                                type: account.type,
+                                provider: account.provider,
+                                providerAccountId: account.providerAccountId,
+                                access_token: account.access_token,
+                                expires_at: account.expires_at,
+                                token_type: account.token_type,
+                                scope: account.scope,
+                                id_token: account.id_token,
+                            },
+                        },
+                    },
+                });
+            }
+
+            if (userCheck && !userCheck.accounts.length) {
+                await prisma.account.create({
+                    data: {
+                        userId: userCheck.id,
+                        type: account.type,
+                        provider: account.provider,
+                        providerAccountId: account.providerAccountId,
+                        access_token: account.access_token,
+                        expires_at: account.expires_at,
+                        token_type: account.token_type,
+                        scope: account.scope,
+                        id_token: account.id_token,
+                    },
+                });
+
+                if (!userCheck.name || !userCheck.image) {
+                    await prisma.user.update({
+                        where: { id: userCheck.id },
+                        data: {
+                            name: user.name,
+                            emailVerified: profile.email_verified,
+                            image: user.image,
+                        },
+                    });
+                }
+            }
+
+            return true;
+        },
         session: ({ session, user }) => ({
             ...session,
             user: {
@@ -50,16 +112,12 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: env.GOOGLE_CLIENT_ID,
             clientSecret: env.GOOGLE_CLIENT_SECRET,
+            // authorization: {
+            //     params: {
+            //         prompt: "consent",
+            //     },
+            // },
         }),
-        /**
-         * ...add more providers here.
-         *
-         * Most other providers require a bit more work than the Discord provider. For example, the
-         * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-         * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-         *
-         * @see https://next-auth.js.org/providers/github
-         */
     ],
 };
 
