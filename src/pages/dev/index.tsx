20a009a58ab3ee501f779081ac91d8b966497c75
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-04 22:45:41 -0700

    Test page for granting admin role

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+
+type AllUsersType = RouterOutputs["users"]["getAll"];
 
 export default function Dev() {
-    const id = "clkelqi7z0003uw23xxw6wdz2";
-    const { data } = api.bookings.getByInvoiceId.useQuery(id.slice(0, 10));
-    console.log(data);
-    return <div></div>;
+    const [users, setUsers] = useState<AllUsersType>([]);
+
+    const ctx = api.useContext();
+    const { data } = api.users.getAll.useQuery();
+    const { mutate } = api.users.createAdmin.useMutation({
+        onSuccess: () => ctx.users.getAll.invalidate(),
+    });
+
+    useEffect(() => {
+        if (data) setUsers(data);
+    }, [data]);
+    return (
+        <div>
+            <button onClick={() => mutate("brown.zaviar@gmail.com")}>
+                Test user create
+            </button>
+
+            {users.map((userObj) => {
+                return (
+                    <div key={userObj.id}>
+                        <div>Id: {userObj.id}</div>
+                        <div>Email: {userObj.email}</div>
+                        <div>Role: {userObj.role} </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
