e0b91bfe43c25de17661a8218f7f0181b49ab181
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-05 03:20:38 -0700

    Apply layout & new home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Layout from "~/components/layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider session={session}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </SessionProvider>
+    );
 };
 
 export default api.withTRPC(MyApp);
