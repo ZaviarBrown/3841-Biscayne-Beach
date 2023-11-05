d0308e6d8a00ea21a5b8bac0564e1a8d520fd8b9
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-05 16:32:33 -0700

    Delete booking

--- src/components/Booking/Delete.tsx
+++ src/components/Booking/Delete.tsx
@@ -1,3 +1,20 @@
-export default function DeleteBooking() {
-    return <div>Delete Booking</div>;
+import { api } from "~/utils/api";
+
+// TODO: Stripe cancellation fee
+
+
+export default function DeleteBooking({ id }: { id: string }) {
+    const ctx = api.useContext();
+    const { mutate } = api.booking.delete.useMutation({
+        onSuccess: () => ctx.booking.invalidate(),
+    });
+
+    return (
+        <button
+            className="m-5 rounded-lg bg-red-500 p-2 text-slate-50 shadow-xl"
+            onClick={() => mutate(id)}
+        >
+            Delete Booking
+        </button>
+    );
 }
