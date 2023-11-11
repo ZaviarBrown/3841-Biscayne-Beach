ef2bc04a5578355edc74c0e9c5a9587d7ed60999
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-11 00:53:48 -0700

    CreateReview form & page

--- src/components/Review/Create.tsx
+++ src/components/Review/Create.tsx
@@ -1,3 +1,38 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { api } from "~/utils/api";
+
 export default function CreateReview() {
-    return <div>Create Review</div>;
+    const router = useRouter();
+
+    const [text, setText] = useState("");
+    const [rating, setRating] = useState(0);
+
+    const { mutate } = api.review.create.useMutation({
+        onSuccess: () => {
+            void router.push("/reviews");
+        },
+    });
+
+    return (
+        <form>
+            <textarea value={text} onChange={(e) => setText(e.target.value)} />
+            <input
+                type="number"
+                value={rating}
+                onChange={(e) => setRating(Number(e.target.value))}
+            />
+            <button
+                onClick={() => {
+                    mutate({
+                        text,
+                        rating,
+                        bookingId: router.query.id as string,
+                    });
+                }}
+            >
+                Submit Review
+            </button>
+        </form>
+    );
 }
---
+++ src/pages/review-your-stay/[id].tsx
@@ -0,0 +1,5 @@
+import CreateReview from "~/components/Review/Create";
+
+export default function ReviewYourStay() {
+    return <CreateReview />;
+}
