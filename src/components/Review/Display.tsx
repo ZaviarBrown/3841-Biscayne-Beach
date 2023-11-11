471c8eebfb9eb5c9adc42351064ffc17f94bb034
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-11 00:54:24 -0700

    Create DisplayReview component

--- src/components/Review/Display.tsx
+++ src/components/Review/Display.tsx
@@ -1,3 +1,15 @@
+import { api } from "~/utils/api";
+import ReviewCard from "./Card";
+
 export default function DisplayReview() {
-    return <div>Display Review</div>;
+    const { data: allReviews } = api.review.getAll.useQuery();
+
+    return (
+        <div className="mt-5 text-2xl">
+            {allReviews &&
+                allReviews.map((review) => (
+                    <ReviewCard {...review} key={review.id} />
+                ))}
+        </div>
+    );
 }
