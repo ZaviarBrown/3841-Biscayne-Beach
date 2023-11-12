a7cdae8828d256df5cc8e5ce353da27f3aa6f062
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-12 14:09:11 -0700

    Basic styling for cards

--- src/components/Booking/Card.tsx
+++ src/components/Booking/Card.tsx
@@ -2,7 +2,7 @@ import type { Booking } from "@prisma/client";
 
 export default function BookingCard({ startDate, endDate }: Booking) {
     return (
-        <div className="m-5 rounded-lg bg-white px-10 py-5 text-slate-800 shadow-3xl">
+        <div className="m-5 flex flex-col items-center rounded-lg bg-white px-10 py-5 text-slate-800 shadow-3xl">
             <p>Arrive: {startDate.toLocaleDateString()}</p>
             <p>Depart: {endDate.toLocaleDateString()}</p>
         </div>
--- src/components/Review/Card.tsx
+++ src/components/Review/Card.tsx
@@ -10,11 +10,21 @@ export default function ReviewCard({
     booking: { startDate },
 }: FullReviewType) {
     return (
-        <div className="m-5 rounded-lg bg-white px-10 py-5 text-slate-800 shadow-3xl">
-            <p>{name}</p>
-            <p>{format(startDate, "MMM yy")}</p>
+        <div className="m-5 flex w-96 flex-col items-center rounded-lg bg-white px-10 py-5 text-slate-800 shadow-3xl">
+            <div className="flex w-full justify-between">
+                <p>{name}</p>
+                <p>{format(startDate, "MMM yy")}</p>
+            </div>
+            <div className="flex gap-3">
+                {Array(rating)
+                    .fill("⭐️")
+                    .map((_, i) => (
+                        <div className="text-blue-500" key={i}>
+                            ⭐️
+                        </div>
+                    ))}
+            </div>
             <p>{text}</p>
-            <div>{rating}</div>
         </div>
     );
 }
