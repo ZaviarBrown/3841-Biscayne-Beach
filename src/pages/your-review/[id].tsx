a08cbe84409784038fd8cbcf8fc75f6601faab44
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-13 11:59:10 -0700

    Optimize query for getOneReview

--- src/pages/your-review/[id].tsx
+++ src/pages/your-review/[id].tsx
@@ -5,7 +5,9 @@ import { api } from "~/utils/api";
 
 export default function YourReview() {
     const router = useRouter();
-    const { data } = api.review.getOne.useQuery(router.query.id as string);
+    const { data } = api.review.getOne.useQuery(router.query.id as string, {
+        enabled: !!router.query.id,
+    });
 
     if (!data) return <h1>Loading...</h1>;
 
