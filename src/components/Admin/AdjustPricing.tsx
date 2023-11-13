bd1c22b46e06d9f977acfa4893d8eceff06eed84
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-13 11:57:21 -0700

    Create AdjustPricing component

---
+++ src/components/Admin/AdjustPricing.tsx
@@ -0,0 +1,8 @@
+export default function AdjustPricing() {
+    return (
+        <form className="m-5 flex w-96 flex-col items-center gap-10 rounded-lg bg-white px-10 py-5 text-slate-800 shadow-3xl">
+            <h1 className="text-3xl">Adjust Pricing</h1>
+            <h2 className="text-xl">Pending...</h2>
+        </form>
+    );
+}
