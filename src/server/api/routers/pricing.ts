24e6e2007dcf42c657f150357f6ea78f8be708d8
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-26 18:22:25 -0700

    Pricing API create, update, delete

--- src/server/api/routers/pricing.ts
+++ src/server/api/routers/pricing.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 import {
     createTRPCRouter,
     publicProcedure,
-    protectedProcedure,
     adminProcedure,
 } from "~/server/api/trpc";
+import { convertDollarsIntoCents } from "~/utils/booking";
 
 export const pricingRouter = createTRPCRouter({
     getAllValidWindows: publicProcedure.query(async ({ ctx }) => {
@@ -33,6 +33,74 @@ export const pricingRouter = createTRPCRouter({
         };
     }),
 
-    // TODO: Create custom priced days
-    // createPrice: adminProcedure.input().mutation(),
+    createWindow: adminProcedure
+        .input(
+            z.object({
+                startDate: z.date(),
+                endDate: z.date(),
+                priceInDollars: z.string(),
+                note: z.string(),
+            })
+        )
+        .mutation(
+            async ({
+                input: { startDate, endDate, priceInDollars, note },
+                ctx,
+            }) => {
+                const price = convertDollarsIntoCents(priceInDollars);
+
+                const newWindow = await ctx.prisma.pricingWindows.create({
+                    data: {
+                        startDate,
+                        endDate,
+                        price,
+                        note,
+                    },
+                });
+
+                return newWindow;
+            }
+        ),
+
+    updateWindow: adminProcedure
+        .input(
+            z.object({
+                id: z.string(),
+                startDate: z.date(),
+                endDate: z.date(),
+                priceInDollars: z.string(),
+                note: z.string(),
+            })
+        )
+        .mutation(
+            async ({
+                input: { id, startDate, endDate, priceInDollars, note },
+                ctx,
+            }) => {
+                const price = convertDollarsIntoCents(priceInDollars);
+
+                const updatedWindow = await ctx.prisma.pricingWindows.update({
+                    where: {
+                        id,
+                    },
+
+                    data: {
+                        startDate,
+                        endDate,
+                        price,
+                        note,
+                    },
+                });
+
+                return updatedWindow;
+            }
+        ),
+
+    deleteWindow: adminProcedure
+        .input(z.string())
+        .mutation(async ({ input: id, ctx }) => {
+            await ctx.prisma.pricingWindows.delete({ where: { id } });
+
+            return "Successfully deleted";
+        }),
 });
