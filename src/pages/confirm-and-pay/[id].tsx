261a2c4521d3486124850c6a3f0e52f07d76d36e
Zaviar Brown <brown.zaviar@gmail.com>
2023-11-26 17:43:05 -0700

    Messy but working pay confirmation screen

--- src/pages/confirm-and-pay/[id].tsx
+++ src/pages/confirm-and-pay/[id].tsx
@@ -8,16 +8,30 @@ import { api } from "~/utils/api";
 import { loadStripe } from "@stripe/stripe-js";
 import { env } from "~/env.mjs";
 import { useEffect, useState } from "react";
+import { useBookingContext } from "~/context/BookingContext";
+
+import type { BookingContextType } from "~/context/BookingContext";
 
 // TODO: getServerSideProps
 
+// TODO: This is a hot mess please refactor this dear god
+
 const stripePromise = loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLIC);
+
+const formatTimeLeft = (time: number): string => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+};
 
 export default function ConfirmAndPay() {
     const router = useRouter();
     const [clientSecret, setClientSecret] = useState("");
 
-    const { data: booking, isLoading } = api.booking.getById.useQuery(
+    const { booking, setBooking, timeLeft } =
+        useBookingContext() as BookingContextType;
+
+    const { data: restoreBooking, isLoading } = api.booking.getById.useQuery(
         router.query.id as string,
         { enabled: !!router.query.id }
     );
@@ -35,17 +49,27 @@ export default function ConfirmAndPay() {
         }
     }, [booking, clientSecret, createCheckout]);
 
+    useEffect(() => {
+        if (!booking && restoreBooking) {
+            setBooking(restoreBooking);
+        }
+    }, [booking, restoreBooking, setBooking]);
+
     if (isLoading) return <div>Loading...</div>;
 
-    if (!booking) {
+    if (!booking && !restoreBooking) {
         void router.push("/");
         return null;
     }
+
+    if (!booking) return <div>Loading...</div>;
 
     if (!clientSecret) return <div>Loading...</div>;
 
     return (
         <div className="flex">
+            <h1 className="text-4xl">{formatTimeLeft(timeLeft)}</h1>
+
             <div className="text-center">
                 <h1 className="text-3xl">Confirm and pay</h1>
                 <BookingCard {...booking} />
