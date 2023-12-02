d8706941dbca04d7e7e94dd88d58d966c86b8d2d
Zaviar Brown <brown.zaviar@gmail.com>
2023-12-01 19:17:41 -0700

    Integrate custom pricing into AdjustPricing component

--- src/components/Admin/AdjustPricing.tsx
+++ src/components/Admin/AdjustPricing.tsx
@@ -1,7 +1,17 @@
+import { api } from "~/utils/api";
+import CreatePricingWindow from "../Pricing/Create";
+
 export default function AdjustPricing() {
+    const { data: pricingWindows, isLoading } =
+        api.pricing.getAllValidWindows.useQuery();
+
+    if (isLoading) return <h1>Loading...</h1>;
+
+    if (!pricingWindows) return null;
+
     return (
         <form className="m-5 flex w-96 flex-col items-center gap-10 rounded-lg bg-white px-10 py-5 text-slate-800 shadow-3xl">
-            <h1 className="text-3xl">Adjust Pricing</h1>
+            <CreatePricingWindow customPrices={pricingWindows.customPrices} />
             <h2 className="text-xl">Pending...</h2>
         </form>
     );
