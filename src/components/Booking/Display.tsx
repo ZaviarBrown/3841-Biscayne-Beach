61dff69d72a84e9b3a1d3946d0e5e73f33bbef21
Zaviar Brown <brown.zaviar@gmail.com>
2023-12-27 06:26:12 -0700

    Style user bookings page

--- src/components/Booking/Display.tsx
+++ src/components/Booking/Display.tsx
@@ -9,26 +9,38 @@ import Link from "next/link";
 export default function DisplayBookings({ userId }: { userId: string }) {
     const { data: userBookings } = api.booking.getByUserId.useQuery(userId);
 
+    if (!userBookings || !userBookings.length)
+        return (
+            <h1 className="m-auto text-4xl text-white">
+                {"You don't have any bookings yet! "}
+                <Link
+                    className="text-blue-200 underline underline-offset-2"
+                    href="/book"
+                >
+                    {"Let's change that"}
+                </Link>
+            </h1>
+        );
+
     return (
-        <div className="mt-5 text-2xl">
-            {userBookings &&
-                userBookings.map((booking) => {
-                    return (
-                        <div key={booking.id}>
-                            {/* 
+        <div className="mt-5 flex text-2xl">
+            {userBookings.map((booking) => {
+                return (
+                    <div key={booking.id}>
+                        {/* 
                             // TODO: Different links depending on booking status
                             */}
-                            <Link href={`/confirm-and-pay/${booking.id}`}>
-                                <BookingCard {...booking} />
-                            </Link>
-                            <DeleteBooking id={booking.id} />
-                            <HasReviewed
-                                bookingId={booking.id}
-                                review={booking.Review}
-                            />
-                        </div>
-                    );
-                })}
+                        {/* <Link href={`/confirm-and-pay/${booking.id}`}> */}
+                        <BookingCard {...booking} />
+                        {/* </Link> */}
+                        
+                        {/* <HasReviewed
+                            bookingId={booking.id}
+                            review={booking.Review}
+                        /> */}
+                    </div>
+                );
+            })}
         </div>
     );
 }
