6c3bfc9a6d195003b3d8fe27a4144047ee628fc6
Zaviar Brown <brown.zaviar@gmail.com>
2023-12-27 06:26:38 -0700

    Setup landing page for parallax scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import type { NextPage } from "next";
 
 import Carousel from "../components/Carousel";
 import HomeDetails from "../components/HomeDetails";
+import GoogleMapsEmbed from "~/components/GoogleMapsEmbed";
+import HomeHook from "~/components/HomeHook";
+import HomeAmenities from "~/components/HomeAmenities";
 
 const Home: NextPage = () => {
     return (
         <>
             <Carousel />
+            <HomeHook />
             <HomeDetails />
+            <HomeAmenities />
+            <GoogleMapsEmbed />
         </>
     );
 };
