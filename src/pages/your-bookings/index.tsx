ebac54346fbfff35dc0ae6a642d55c3eb01b601f
Zaviar Brown <brown.zaviar@gmail.com>
2023-12-27 06:24:58 -0700

    Style user bookings page

--- src/pages/your-bookings/index.tsx
+++ src/pages/your-bookings/index.tsx
@@ -1,14 +1,20 @@
 import { useSession } from "next-auth/react";
 import DisplayBookings from "~/components/Booking/Display";
+import NavBarSpacer from "~/components/NavBarSpacer";
 
 export default function UserBookingsPage() {
     const { data: session } = useSession();
 
     if (!session)
         return (
-            <h1 className="text-7xl">
+            <h1 className="m-auto text-5xl text-white">
                 You must be signed in to view this page
             </h1>
         );
-    return <DisplayBookings userId={session.user.id} />;
+    return (
+        <>
+            <NavBarSpacer />
+            <DisplayBookings userId={session.user.id} />
+        </>
+    );
 }
