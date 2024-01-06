f2c542af6dde872ddff60d0eed8664aac7139ccf
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-06 03:33:28 -0700

    Created InfoCard for AdjustPricing

--- src/components/Admin/AdjustPricing.tsx
+++ src/components/Admin/AdjustPricing.tsx
@@ -9,5 +9,49 @@ export default function AdjustPricing() {
 
     if (!pricingWindows) return null;
 
-    return <CreatePricingWindow customPrices={pricingWindows.customPrices} />;
+    return (
+        <div className="flex h-full flex-col gap-5">
+            <InfoCard />
+
+            <CreatePricingWindow customPrices={pricingWindows.customPrices} />
+        </div>
+    );
 }
+
+const InfoCard = () => {
+    return (
+        <div className="flex w-full items-center justify-evenly rounded-lg bg-white p-3 text-center shadow-lg">
+            <div>
+                <p className="m-auto h-8 w-8 rounded-full bg-green-500 bg-opacity-70 text-2xl">
+                    {"💰"}
+                </p>
+                <span className="text-2xl font-bold">$0.00</span>
+                <p className="text-sm text-slate-500">All-Time Revenue</p>
+            </div>
+            <span className="h-20 border-r" />
+            <div>
+                <p className="m-auto h-8 w-8 rounded-full bg-green-500 bg-opacity-70 text-2xl">
+                    {"💰"}
+                </p>
+                <span className="text-2xl font-bold">$0.00</span>
+                <p className="text-sm text-slate-500">Revenue This Year</p>
+            </div>
+            <span className="h-20 border-r" />
+            <div>
+                <p className="m-auto h-8 w-8 rounded-full bg-red-500 bg-opacity-70 text-2xl">
+                    {"💰"}
+                </p>
+                <span className="text-2xl font-bold">$0.00</span>
+                <p className="text-sm text-slate-500">All-Time Refunds</p>
+            </div>
+            <span className="h-20 border-r" />
+            <div>
+                <p className="m-auto h-8 w-8 rounded-full bg-red-500 bg-opacity-70 text-2xl">
+                    {"💰"}
+                </p>
+                <span className="text-2xl font-bold">$0.00</span>
+                <p className="text-sm text-slate-500">Refunds This Year</p>
+            </div>
+        </div>
+    );
+};
