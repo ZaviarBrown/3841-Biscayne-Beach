963577fe19ea9aed63969b4860483f1bd6bc3d8f
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-06 03:11:16 -0700

    Adjust loading card bg-color to better match new design

--- src/components/Gallery/Loading.tsx
+++ src/components/Gallery/Loading.tsx
@@ -1,5 +1,5 @@
 export default function LoadingCard() {
     return (
-        <div className="h-80 w-full animate-pulse rounded-lg bg-slate-600 shadow-3xl" />
+        <div className="h-80 w-full animate-pulse rounded-lg border-2 border-slate-200 bg-slate-600 shadow-3xl" />
     );
 }
