c0c0ec547474d647002f2a64af5926ab3fa5d411
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-07 02:58:51 -0700

    GalleryCard update for getStaticProps

--- src/components/Gallery/Card.tsx
+++ src/components/Gallery/Card.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
 export default function GalleryCard({
-    url,
-    i,
+    src,
+    alt,
     load,
 }: {
-    url: string;
-    i: number;
+    src: string;
+    alt: string;
     load: boolean;
 }) {
     return (
         <div className="relative h-80 w-full overflow-hidden rounded-lg border-2 border-slate-200 bg-slate-500 shadow-3xl duration-300 hover:-translate-y-1">
             <Image
-                src={`/allPhotos/${url}`}
-                alt={`Artwork ${i + 1}`}
+                src={src}
+                alt={alt}
                 fill
                 className={`object-cover ${
                     load ? "opacity-100" : "opacity-0"
