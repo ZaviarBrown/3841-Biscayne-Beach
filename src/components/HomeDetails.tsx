9656a7365f6a6381020dd348bc52ab0a7278cefe
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-07 03:03:02 -0700

    Refactor HomeDetails for SSR

--- src/components/HomeDetails.tsx
+++ src/components/HomeDetails.tsx
@@ -1,26 +1,63 @@
+import type { HomeDetailsType } from "~/pages";
 import ParallaxImage from "./ParallaxImage";
 
-export default function HomeDetails() {
+const ParallaxDetailScene = ({
+    src,
+    alt,
+    side,
+    text,
+}: {
+    src: string;
+    alt: string;
+    side: number;
+    text: string;
+}) => {
+    const textArr = text.split(".");
+
     return (
         <>
-            <ParallaxImage
-                src="/landingPhotos/exterior_overhead.jpg"
-                alt="New Kitchen"
-            >
+            <ParallaxImage src={src} alt={alt}>
                 <div className="flex h-full w-full">
-                    <div className="flex w-2/3 flex-col items-center justify-center gap-3 bg-black bg-opacity-70 text-center text-white">
-                        <h1 className="w-1/2 text-6xl">
-                            Fits your family, and then some
-                        </h1>
-                        <p className="w-1/2 text-2xl">5 Bedrooms</p>
-                        <p className="w-1/2 text-2xl">4 Bathrooms</p>
-                        <p className="w-1/2 text-2xl">
-                            Sleeps 16 adults + 4 kids
-                        </p>
+                    {side === 1 && <div className="w-2/3" />}
+                    <div
+                        className={`flex w-1/3 flex-col items-center justify-center gap-5 backdrop-blur-sm ${
+                            side === 0 ? "border-r" : "border-l"
+                        } border-white bg-black bg-opacity-80 p-5 text-center text-white`}
+                    >
+                        {textArr.map((text, i) => {
+                            return (
+                                <p
+                                    key={i}
+                                    className="w-full break-words p-3 text-2xl"
+                                >
+                                    {text}.
+                                </p>
+                            );
+                        })}
                     </div>
-                    <div className="w-1/3" />
+                    {side === 0 && <div className="w-2/3" />}
                 </div>
             </ParallaxImage>
+        </>
+    );
+};
+
+export default function HomeDetails({
+    homeDetails,
+}: {
+    homeDetails: HomeDetailsType[];
+}) {
+    return (
+        <>
+            {homeDetails.map((details, i) => {
+                return (
+                    <ParallaxDetailScene
+                        key={details.alt}
+                        {...details}
+                        side={i % 2}
+                    />
+                );
+            })}
         </>
     );
 }
