c43b9d79f5a60b00244a64ecb4ca605f5edbe32c
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 04:57:38 -0700

    Update booking card with status and provide new props to DeleteBooking

--- src/components/Booking/Card.tsx
+++ src/components/Booking/Card.tsx
@@ -1,14 +1,43 @@
 import type { Booking } from "@prisma/client";
 import DeleteBooking from "./Delete";
+import { useEffect, useState } from "react";
 
-export default function BookingCard({ startDate, endDate, id }: Booking) {
+export default function BookingCard({
+    startDate,
+    endDate,
+    id,
+    userId,
+    name,
+    paymentId,
+    price,
+    status,
+}: Booking) {
+    const [isFuture, setIsFuture] = useState(true);
+
+    useEffect(() => {
+        if (startDate.getTime() < Date.now()) {
+            setIsFuture(false);
+        }
+    }, [startDate]);
+
     return (
         <div className="m-5 flex items-center rounded-lg bg-white p-5 text-2xl text-slate-800 shadow-3xl">
             <div className="flex flex-col text-left">
                 <p>Check-in: {startDate.toLocaleDateString()}</p>
                 <p>Check-out: {endDate.toLocaleDateString()}</p>
+                <p>Payment status: {status}</p>
             </div>
-            <DeleteBooking id={id} />
+            {isFuture && (
+                <DeleteBooking
+                    id={id}
+                    userId={userId}
+                    name={name}
+                    price={price}
+                    paymentId={paymentId}
+                    startDate={startDate}
+                    endDate={endDate}
+                />
+            )}{" "}
         </div>
     );
 }
