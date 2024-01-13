1e333f8906c01fcd6eab74eb7c930f0032d4a878
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 02:53:59 -0700

    Handle role change on form submission instead of on button click

--- src/components/Admin/GrantRole.tsx
+++ src/components/Admin/GrantRole.tsx
@@ -8,12 +8,19 @@ export default function GrantRole() {
 
     const { mutate } = api.user.createNewAdmin.useMutation({
         onSuccess: () => {
+            setEmail("");
             void update();
         },
     });
 
     return (
-        <form className="flex flex-col items-center gap-10 rounded-lg p-5 text-slate-800 ">
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                mutate(email);
+            }}
+            className="flex flex-col items-center gap-10 rounded-lg p-5 text-slate-800 "
+        >
             <h1 className="text-center text-3xl">Pre-create a User as Admin</h1>
             <input
                 className="rounded-lg p-2 outline outline-1 outline-black"
@@ -21,13 +28,7 @@ export default function GrantRole() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
             />
-            <button
-                className="rounded-lg bg-green-500 px-3 py-2 text-xl text-white shadow-3xl"
-                onClick={(e) => {
-                    e.preventDefault();
-                    mutate(email);
-                }}
-            >
+            <button className="rounded-lg bg-green-500 px-3 py-2 text-xl text-white shadow-lg duration-200 hover:scale-105 hover:bg-green-600">
                 Create New Admin
             </button>
         </form>
