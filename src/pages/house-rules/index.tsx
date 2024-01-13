b1bcbb00b0a924dd29d55a0fd13f4d81227965b4
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 03:04:47 -0700

    Small HouseRules styling changes & Cancelation => Cancellation

--- src/pages/house-rules/index.tsx
+++ src/pages/house-rules/index.tsx
@@ -1,56 +1,66 @@
+import Link from "next/link";
 import NavBarSpacer from "~/components/NavBarSpacer";
 
 export default function HouseRules() {
     const rules = [
-        "No pets",
-        "No smoking on property",
-        "No parties / gatherings",
-        "No loud noises or music",
-        "No fishing in the ponds",
-        "No bait inside the house",
-        "No banners, signs or flags",
-        "No firearms",
-        "No illegal substances",
-        "Park in the driveway or under the house (concrete) only",
+        "No pets.",
+        "No smoking on property.",
+        "No parties / gatherings.",
+        "No loud noises or music.",
+        "No fishing in the ponds.",
+        "No bait inside the house.",
+        "No banners, signs or flags.",
+        "No firearms.",
+        "No illegal substances.",
+        "Park in the driveway or under the house (concrete) only.",
     ];
 
     const checkout = [
-        "Check-in at 3pm, check-out at 11am",
-        "Please start the dishwasher for us",
-        "Please remove everything you brought including refrigerated food, trash, and consumable products",
-        "Upon checkout, please bundle used bedclothes on the floor and we will wash them for the next guest",
-        "Please leave things as you found them",
-        "All trash should be taken out in bags and tied up",
-        "Please do not throw loose trash in the cans and do not overfill the cans",
-        "If you have more trash than the cans hold, leave it in bags tied up on the deck by the front steps",
+        "Check-in at 3pm, check-out at 11am.",
+        "Please start the dishwasher for us.",
+        "Please remove everything you brought including refrigerated food, trash, and consumable products.",
+        "Upon checkout, please bundle used bedclothes on the floor and we will wash them for the next guest.",
+        "Please leave things as you found them.",
+        "All trash should be taken out in bags and tied up.",
+        "Please do not throw loose trash in the cans and do not overfill the cans.",
+        "If you have more trash than the cans hold, leave it in bags tied up on the deck by the front steps.",
     ];
 
     const policies = [
-        "By booking with us, you agree to follow all rules listed on this page",
-        "Purchases through our platform are 100% handled by Stripe; we never see your card info",
-        "There is a 5 night minimum for every booking",
-        "You must pay for the entirety of your stay at the time of booking",
-        "Stripe charges a card processing fee for every transaction",
-        "We cover this fee when you book with us, but this fee is non-refundable for cancellations",
-        "Once selected, your dates are reserved for 15 minutes so you can complete your purchase",
-        "If you do not complete your purchase during that time, your booking will be deleted",
-        'If you wish to cancel a booking, you can do so through the "Your Bookings" page',
-        "Bookings canceled with more than 10 days notice will receive a full refund",
-        "Bookings canceled with less than 10 days notice will receive a refund equivalent to 50% of the total booking cost",
-        "Bookings canceled with less than 3 days notice will receive no refund",
+        "By booking with us, you agree to follow all rules listed on this page.",
+        "Purchases through our platform are 100% handled by Stripe; we never see your card info.",
+        "Stripe charges a card processing fee for every transaction. We cover this fee when you book with us, but this fee is non-refundable for cancellations. Any mention of refunds in this section include this fee.",
+        "Once selected, your dates are reserved for 15 minutes so you can complete your purchase. If you do not complete your purchase during that time, your booking will be deleted and you will not be charged.",
+        "There is a 5 night minimum for every booking.",
+        "You must pay for the entirety of your stay at the time of booking.",
+        "Bookings cancelled with more than 14 days notice will receive a full refund.",
+        "Bookings cancelled with at least 3 days notice will receive a refund equivalent to 50% of the total booking cost.",
+        "Bookings cancelled with less than 3 days notice will receive no refund.",
+        <>
+            If you wish to cancel a Booking, you can do so on the{" "}
+            {
+                <Link
+                    className="text-blue-500 hover:underline"
+                    href={"/your-bookings"}
+                >
+                    Your Bookings
+                </Link>
+            }{" "}
+            page.
+        </>,
     ];
 
     return (
         <>
             <NavBarSpacer />
 
-            <div className="m-5 flex flex-col items-center gap-5 p-5">
+            <div className="m-5 flex flex-col items-center gap-10 p-5">
                 <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-2xl text-slate-800 shadow-3xl">
                     <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-3xl text-white">
                         Please follow all Neighborhood Rules & please be
                         considerate of our neighbors right to peace and quiet.
                     </h1>
-                    <div className="m-auto w-4/5">
+                    <div className="m-auto w-4/5 p-5">
                         <ul className="grid grid-cols-2 gap-x-10">
                             {rules.map((rule, i) => (
                                 <li
@@ -68,7 +78,7 @@ export default function HouseRules() {
                         Please follow checkout procedures and leave the house
                         like you found it.
                     </h1>
-                    <div className="m-auto w-4/5">
+                    <div className="m-auto w-4/5 p-5">
                         <ul className="grid grid-cols-2 gap-x-10">
                             {checkout.map((procedure, i) => (
                                 <li
@@ -83,10 +93,10 @@ export default function HouseRules() {
                 </div>
                 <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-2xl text-slate-800 shadow-3xl">
                     <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-3xl text-white">
-                        Please note our Booking & Cancelation policies before
+                        Please note our Booking & Cancellation policies before
                         purchasing your booking.
                     </h1>
-                    <div className="m-auto w-4/5">
+                    <div className="m-auto w-4/5 p-5">
                         <ul className="grid grid-cols-2 gap-x-10">
                             {policies.map((policy, i) => (
                                 <li
