7fd0496fa3d4d03e4c8449f1880d774a2006c5c2
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 15:43:01 -0700

    Muti-layered DeleteBooking overhaul

--- src/components/Booking/Delete.tsx
+++ src/components/Booking/Delete.tsx
@@ -5,25 +5,35 @@ import AdminConfirmDeleteBookingModal from "../Modal/AdminConfirmDeleteBookingMo
 import { useEffect, useState } from "react";
 import { add, isBefore } from "date-fns";
 import { env } from "~/env.mjs";
+import { useSession } from "next-auth/react";
+import UserConfirmDeleteBookingModal from "../Modal/UserConfirmDeleteBookingModal";
+import DeleteFreeBookingModal from "../Modal/DeleteFreeBookingModal";
+import { render } from "@react-email/render";
+import CancelConfirmationEmail from "~/emails/CancelConfirmation";
 
 export default function DeleteBooking({
     id,
     name,
+    email,
     userId,
     price,
     paymentId,
     startDate,
     endDate,
+    status,
 }: {
     id: string;
     name: string;
+    email: string;
     userId: string;
     price: number;
     paymentId: string | null;
     startDate: Date;
     endDate: Date;
+    status: string;
 }) {
     const [refundPrice, setRefundPrice] = useState(0);
+    const { data: session } = useSession();
     const ctx = api.useContext();
     const router = useRouter();
     const { mutate } = api.booking.delete.useMutation({
@@ -35,7 +45,7 @@ export default function DeleteBooking({
 
     useEffect(() => {
         const maxRefund = Math.floor(
-            price * Number(env.NEXT_PUBLIC_STRIPE_FEE)
+            price * Number(env.NEXT_PUBLIC_REFUND_FEE)
         );
         const today = new Date();
 
@@ -49,6 +59,61 @@ export default function DeleteBooking({
         else return setRefundPrice(0);
     }, [startDate, price]);
 
+    const handleSubmit = () => {
+        const bookingInfo = { id, userId, refundPrice, paymentId };
+        const emailInfo = {
+            to: email,
+            subject: "Cancellation Confirmation",
+            html: render(
+                <CancelConfirmationEmail
+                    name={name}
+                    email={email}
+                    startDate={startDate}
+                    endDate={endDate}
+                />
+            ),
+        };
+
+        mutate({ bookingInfo, emailInfo });
+    };
+
+    // TODO: Refactor - move conditional logic up a folder layer
+
+    if (session && session.user.id === userId) {
+        if (status === "admin")
+            return (
+                <OpenModalButton
+                    modalComponent={
+                        <DeleteFreeBookingModal
+                            name={"my"}
+                            startDate={startDate}
+                            endDate={endDate}
+                        />
+                    }
+                    buttonText="Cancel Booking"
+                    className="m-auto w-fit rounded-lg bg-red-500 px-5 py-2 text-slate-50 shadow-xl duration-200 hover:scale-105 hover:bg-red-600 "
+                    onModalSubmit={handleSubmit}
+                    onModalClose={() => null}
+                />
+            );
+
+        return (
+            <OpenModalButton
+                modalComponent={
+                    <UserConfirmDeleteBookingModal
+                        refundPrice={refundPrice}
+                        startDate={startDate}
+                        endDate={endDate}
+                    />
+                }
+                buttonText="Cancel Booking"
+                className="m-auto w-fit rounded-lg bg-red-500 px-5 py-2 text-slate-50 shadow-xl duration-200 hover:scale-105 hover:bg-red-600 "
+                onModalSubmit={handleSubmit}
+                onModalClose={() => null}
+            />
+        );
+    }
+
     return (
         <OpenModalButton
             modalComponent={
@@ -57,12 +122,11 @@ export default function DeleteBooking({
                     refundPrice={refundPrice}
                     startDate={startDate}
                     endDate={endDate}
-                    paymentId={paymentId}
                 />
             }
             buttonText="Cancel Booking"
             className="m-auto w-fit rounded-lg bg-red-500 px-5 py-2 text-slate-50 shadow-xl duration-200 hover:scale-105 hover:bg-red-600 "
-            onModalSubmit={() => mutate({ id, refundPrice, userId, paymentId })}
+            onModalSubmit={handleSubmit}
             onModalClose={() => null}
         />
     );
