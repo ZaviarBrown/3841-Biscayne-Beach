5e6bc179e9c30ce61bacb6af8843d3f73a786eb1
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 03:08:48 -0700

    Nodemailer config

---
+++ src/utils/email.ts
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+import { env } from "~/env.mjs";
+
+type EmailDetailsType = {
+    html: string;
+    subject: string;
+    replyTo?: string;
+    from?: string;
+    to?: string;
+};
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: env.EMAIL_USER,
+        pass: env.EMAIL_PASSWORD,
+    },
+});
+
+export const sendEmail = async ({
+    html,
+    subject,
+    replyTo,
+    from,
+    to,
+}: EmailDetailsType) =>
+    transporter.sendMail({
+        // to: to || "contact@3841biscaynebeach.com",
+        // from: from || "contact@3841biscaynebeach.com",
+        // replyTo: replyTo || "contact@3841biscaynebeach.com",
+        to: to || "zaviar.brown@gmail.com",
+        from: from || "zaviar.brown@gmail.com",
+        replyTo: replyTo || "zaviar.brown@gmail.com",
+        subject,
+        html,
+    });
