a23bbb3ccb1bdc76dd5a9597810023d4e02bef39
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 18:59:07 -0700

    We'll do something with this eventually

---
+++ src/pages/attributions/index.tsx
@@ -0,0 +1,20 @@
+export default function Attributions() {
+    return (
+        <ul>
+            <li>
+                <p>
+                    <a
+                        target="_blank"
+                        href="https://icons8.com/icon/VhqD3P4a8KMP/island-on-water"
+                    >
+                        Island
+                    </a>{" "}
+                    icon by{" "}
+                    <a target="_blank" href="https://icons8.com">
+                        Icons8
+                    </a>
+                </p>
+            </li>
+        </ul>
+    );
+}
