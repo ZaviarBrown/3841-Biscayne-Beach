0119ca769053bd0d7b2b1f6440aa1771479c4254
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 19:02:45 -0700

    Setup plan for next time

--- notes.ts
+++ notes.ts
@@ -1,80 +1,25 @@
-/** Next
+/** Todo's
  *
- * 
-    * !!!Checkout Timer!!!
-        * On first load of dates, check createdAt & pending
-        * Delete pending bookings after 30min
+ * ? Gallery page:
+ * - Making each photo clickable to display a larger version
  *
-    * ? Gallery page: 
-        * - Making each photo clickable to display a larger version 
- *
-    * ? Admin page:
-        * - Allow booking creation without payment
-        * - Adding a past vs future bookings breakdown
- * 
- * Double check stripe
- * Aria label EVERYTHING
+ * Error handling
+ * More Stripe checks
  * Make Admin quick-views functional
- * Redesign db to store canceled bookings
- * 
-*/
+ * Aria label EVERYTHING
+ * Send a message to the user that we got their support email
+ * House details
+ * Reviews
+ * Different name & email options when booking
+ * Number of occupants when booking
+ * Make prices reusable and (maybe) not stored on the booking
+ *
+ */
 
 /**
- *
- *
- *
- * Cancellation policy
- * - 10 days - 6%
- * - 50% refund
- * - Less than 3 days to date no refund
- * - Disasters - full refund
- *
- * House rules/description
- * - No pets
- * - No parties
- * - This means no cleaning fees
- *
- * Admin page
- *
- * Google maps
- * - Different design
-	- NavBar
-- Contact us
-- House details
-- Reviews
-- Pricing details
-- Admin details
-- Google maps api
-- Privacy Policy
-- Terms of Service
-    - Don't discriminate
-    - We reserve the right
-- No refunds if you get kicked off the property
-
-Show 6% tax (8.5%?)
-
-Contact open to anyone
-    - Lynn & Steve & Kristen
-    - Show it in the site
-    - Respond through the site
-    - Use the person's email who responded
-
-Price per day edit
-    - Steve & Lynn & Kristen
-    - Manage cancellations
-
-Add nights, contact us for less nights
-
-Business account for google reviews
-
-House Rules tab
-    - Add disclaimer before booking
-
-Add google maps
+Fun Idea for later!
 
 Custom Classes for tRPC
 User
 	- getReviews
-	- 
-
  */
