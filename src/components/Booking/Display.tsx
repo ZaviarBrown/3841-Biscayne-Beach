6132f2ab2e57339d6523161951b62c568aca40a9
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 19:48:02 -0700

    Debugging on main, yet again. Except this time it's bad

--- src/components/Booking/Display.tsx
+++ src/components/Booking/Display.tsx
@@ -1,18 +1,6 @@
 import { api } from "~/utils/api";
 import BookingCard from "./Card";
-// import HasReviewed from "./HasReviewed";
 import Link from "next/link";
-
-// TODO: Order
-
-<div className="mt-3 flex w-[650px] flex-col items-center gap-3 self-center rounded-lg   text-center text-white shadow-3xl">
-    <h2 className="text-5xl">Select your dates</h2>
-    <p className="text-xl">
-        Please NOTE: When you rent directly from us, this property does NOT tack
-        on SERVICE FEES; CLEANING FEES; BOOKING FEES, CONVENIENCE FEES or the
-        like. Cleaning, booking, and service is included in the total cost.
-    </p>
-</div>;
 
 export default function DisplayBookings({ userId }: { userId: string }) {
     const { data: userBookings } = api.booking.getByUserId.useQuery(userId);
