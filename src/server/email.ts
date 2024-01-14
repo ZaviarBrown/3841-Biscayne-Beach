8d23e756468dff85b23da30d3abe534e217c1545
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-13 20:29:48 -0700

    Testing a promise based solution for nodemailer

--- src/server/email.ts
+++ src/server/email.ts
@@ -24,8 +24,21 @@ export const sendEmail = async ({
     replyTo,
     from,
     to,
-}: EmailDetailsType) =>
-    transporter.sendMail({
+}: EmailDetailsType) => {
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+    
+    return await transporter.sendMail({
         to: to || "contact@3841biscaynebeach.com",
         from: from || "contact@3841biscaynebeach.com",
         replyTo: replyTo || "contact@3841biscaynebeach.com",
@@ -35,3 +48,60 @@ export const sendEmail = async ({
         subject,
         html,
     });
+};
+
+// const nodemailer = require("nodemailer");
+
+// export default async (req, res) => {
+//     const { firstName, lastName, email, message } = JSON.parse(req.body);
+
+//     const transporter = nodemailer.createTransport({
+//         port: 465,
+//         host: "smtp.gmail.com",
+//         auth: {
+//             user: "myEmail@gmail.com",
+//             pass: "password",
+//         },
+//         secure: true,
+//     });
+
+//     await new Promise((resolve, reject) => {
+//         // verify connection configuration
+//         transporter.verify(function (error, success) {
+//             if (error) {
+//                 console.log(error);
+//                 reject(error);
+//             } else {
+//                 console.log("Server is ready to take our messages");
+//                 resolve(success);
+//             }
+//         });
+//     });
+
+//     const mailData = {
+//         from: {
+//             name: `${firstName} ${lastName}`,
+//             address: "myEmail@gmail.com",
+//         },
+//         replyTo: email,
+//         to: "recipient@gmail.com",
+//         subject: `form message`,
+//         text: message,
+//         html: `${message}`,
+//     };
+
+//     await new Promise((resolve, reject) => {
+//         // send mail
+//         transporter.sendMail(mailData, (err, info) => {
+//             if (err) {
+//                 console.error(err);
+//                 reject(err);
+//             } else {
+//                 console.log(info);
+//                 resolve(info);
+//             }
+//         });
+//     });
+
+//     res.status(200).json({ status: "OK" });
+// };
