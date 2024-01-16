73f139d9cc4a4911cdc2021beaf42b9f092f9985
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-16 12:09:00 -0700

    Reverting error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,6 @@ import { BookingContextProvider } from "~/context/BookingContext";
 import { ScrollContextProvider } from "~/context/ScrollContext";
 import { Modal, ModalContextProvider } from "~/context/ModalContext";
 import "default-passive-events";
-import React, { ErrorInfo } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
@@ -22,9 +21,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
                     <BookingContextProvider>
                         <Modal />
                         <Layout>
-                            <ErrorBoundary>
-                                <Component {...pageProps} />
-                            </ErrorBoundary>
+                            <Component {...pageProps} />
                         </Layout>
                     </BookingContextProvider>
                 </ModalContextProvider>
@@ -32,44 +29,5 @@ const MyApp: AppType<{ session: Session | null }> = ({
         </SessionProvider>
     );
 };
-
-class ErrorBoundary extends React.Component {
-    constructor(props: React.Component) {
-        super(props);
-
-        // Define a state variable to track whether is an error or not
-        this.state = { hasError: false };
-    }
-    static getDerivedStateFromError(error: Error) {
-        // Update state so the next render will show the fallback UI
-
-        return { hasError: true };
-    }
-    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-        // You can use your own error logging service here
-        console.log({ error, errorInfo });
-    }
-    render() {
-        // Check if the error is thrown
-        if (this.state.hasError) {
-            // You can render any custom fallback UI
-            return (
-                <div>
-                    <h2>Oops, there is an error!</h2>
-                    <button
-                        type="button"
-                        onClick={() => this.setState({ hasError: false })}
-                    >
-                        Try again?
-                    </button>
-                </div>
-            );
-        }
-
-        // Return children components in case of no error
-
-        return this.props.children;
-    }
-}
 
 export default api.withTRPC(MyApp);
