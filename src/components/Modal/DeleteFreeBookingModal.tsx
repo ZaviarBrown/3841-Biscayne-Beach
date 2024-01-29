5409182e41465dfb34338d1ef534b323bd9b0899
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-28 20:12:32 -0700

    Update styling for DeleteFreeBookingModal

--- src/components/Modal/DeleteFreeBookingModal.tsx
+++ src/components/Modal/DeleteFreeBookingModal.tsx
@@ -15,36 +15,42 @@ export default function DeleteFreeBookingModal({
     const [confirmIrreversible, setConfirmIrreversible] = useState(false);
 
     return (
-        <div className="flex h-full flex-col items-center justify-between overflow-hidden overflow-y-auto">
+        <div className="flex flex-col items-center justify-between overflow-hidden overflow-y-auto">
             <div className="mx-5 flex w-full justify-center rounded-t-2xl border-b border-slate-400 bg-slate-200 p-5 font-bold text-red-600">
                 <h1 className="text-4xl">WARNING!</h1>
             </div>
-            <div className="mx-5 flex h-full flex-col justify-around">
+            <div className="mx-5 flex min-h-60 flex-col justify-around">
                 <h2 className="text-center text-3xl">
                     This action is PERMANENT
                 </h2>
 
-                <label className="w-fit text-xl">
+                <div className="flex p-3">
                     <input
                         checked={confirmIrreversible}
                         onChange={(e) =>
                             setConfirmIrreversible(e.target.checked)
                         }
                         type="checkbox"
-                        className="mx-5"
+                        className="mx-3"
+                        id="irreversible"
                     />
-                    {`I understand that this CANNOT be undone.`}
-                </label>
+                    <label htmlFor="irreversible" className="w-fit text-xl">
+                        {`I understand that this CANNOT be undone.`}
+                    </label>
+                </div>
 
-                <label className="w-fit text-xl">
+                <div className="flex p-3">
                     <input
                         checked={confirmUser}
                         onChange={(e) => setConfirmUser(e.target.checked)}
                         type="checkbox"
-                        className="mx-5"
+                        className="mx-3"
+                        id="user"
                     />
-                    {`I verified that booking for ${name} from ${startDate.toLocaleDateString()} to ${endDate.toLocaleDateString()} is the correct booking to delete.`}
-                </label>
+                    <label htmlFor="user" className="w-fit text-xl">
+                        {`I verified that booking for ${name} from ${startDate.toLocaleDateString()} to ${endDate.toLocaleDateString()} is the correct booking to delete.`}
+                    </label>
+                </div>
             </div>
             <div className="mt-5 flex w-full justify-around rounded-b-2xl border-t border-slate-400 bg-slate-200 p-5 font-bold">
                 <button
