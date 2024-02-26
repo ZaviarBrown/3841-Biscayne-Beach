486c1942cf5f8798ea6ba9d9dd791f7b179f36ca
Zaviar Brown <brown.zaviar@gmail.com>
2024-02-25 19:13:50 -0700

    Create separate Navs for Mobile vs Desktop

---
+++ src/components/Home/Mobile/NavBar.tsx
@@ -0,0 +1,128 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import NavBarSpacer from "../NavBarSpacer";
+
+export default function MobileNavBar({
+    hideNav,
+    pathname,
+    showMenu,
+    setShowMenu,
+}: {
+    hideNav: boolean;
+    pathname: string;
+    showMenu: boolean;
+    setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+    const { data: session } = useSession();
+
+    return (
+        <nav
+            className={`fixed left-0 right-0 top-0 z-50 flex h-10 transform items-center justify-between gap-5  p-2 text-center text-lg text-white ${
+                !showMenu && hideNav ? "-translate-y-full" : "translate-y-0"
+            } transition-transform duration-700 `}
+            aria-label="Main Navigation"
+        >
+            <Link
+                className="z-50 min-w-fit text-lg font-semibold"
+                href="/"
+                aria-label="Home"
+            >
+                Sexy Beaches
+            </Link>
+            <div
+                className={`absolute left-0 top-0 bg-black bg-opacity-50 backdrop-blur transition-all duration-700 ease-out ${
+                    showMenu ? "h-screen" : "h-10"
+                } w-full border-b border-white`}
+            >
+                <NavBarSpacer />
+                <ul
+                    className={`${
+                        showMenu
+                            ? "opacity-100 delay-75 duration-500"
+                            : "pointer-events-none opacity-0 delay-0 duration-500 ease-out"
+                    } flex flex-col items-center gap-8 text-left transition-all`}
+                >
+                    <li className={`${""}`}>
+                        <Link className="group relative" href="/gallery">
+                            Gallery
+                            <span
+                                className={`absolute -bottom-0.5 left-0 h-0.5 bg-white ${
+                                    pathname === "/gallery"
+                                        ? "w-full"
+                                        : "w-0 transition-all duration-200 group-hover:w-full"
+                                }`}
+                            ></span>
+                        </Link>
+                    </li>
+                    <li className={`${""}`}>
+                        <Link className="group relative" href="/house-rules">
+                            House Rules
+                            <span
+                                className={`absolute -bottom-0.5 left-0 h-0.5 bg-white ${
+                                    pathname === "/house-rules"
+                                        ? "w-full"
+                                        : "w-0 transition-all duration-200 group-hover:w-full"
+                                }`}
+                            ></span>
+                        </Link>
+                    </li>
+                    <li className={`${""}`}>
+                        <Link className="group relative" href="/book">
+                            Book Now
+                            <span
+                                className={`absolute -bottom-0.5 left-0 h-0.5  bg-white ${
+                                    pathname === "/book"
+                                        ? "w-full"
+                                        : "w-0 transition-all duration-200 group-hover:w-full"
+                                }`}
+                            ></span>
+                        </Link>
+                    </li>
+                    {session && (
+                        <li className={`${""}`}>
+                            <Link
+                                className="group relative"
+                                href="/your-bookings"
+                            >
+                                Your Bookings
+                                <span
+                                    className={`absolute -bottom-0.5 left-0 h-0.5  bg-white ${
+                                        pathname === "/your-bookings"
+                                            ? "w-full"
+                                            : "w-0 transition-all duration-200 group-hover:w-full"
+                                    }`}
+                                ></span>
+                            </Link>
+                        </li>
+                    )}
+                    {session && session.user.role === "admin" && (
+                        <li className={`${""}`}>
+                            <Link className="group relative" href="/admin">
+                                Admin
+                                <span
+                                    className={`absolute -bottom-0.5 left-0 h-0.5  bg-white ${
+                                        pathname === "/admin"
+                                            ? "w-full"
+                                            : "w-0 transition-all duration-200 group-hover:w-full"
+                                    }`}
+                                ></span>
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <button
+                onClick={() => setShowMenu((prev) => !prev)}
+                className="z-50 flex h-6 min-w-fit items-center justify-between rounded-full border px-2 py-[14px] text-base transition-all duration-200 hover:scale-105"
+            >
+                <Image
+                    src="/assets/island.png"
+                    alt="Island icon by icons8.com"
+                    width={22}
+                    height={22}
+                />
+            </button>
+        </nav>
+    );
+}
