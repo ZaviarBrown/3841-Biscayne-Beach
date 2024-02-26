ea2855a9dfbe0dee8f22fe75a15471b951ebbf02
Zaviar Brown <brown.zaviar@gmail.com>
2024-02-26 13:42:09 -0700

    Style Footer for Mobile

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useMobileContext } from "~/context/MobileContext";
 
 const Footer = () => {
     const { pathname } = useRouter();
     const [hideFooter, setHideFooter] = useState(false);
+    const { isMobile } = useMobileContext();
 
     useEffect(() => {
         if (pathname.startsWith("/confirm-and-pay")) {
@@ -17,9 +19,50 @@ const Footer = () => {
 
     if (hideFooter) return null;
 
+    if (isMobile)
+        return (
+            <>
+                <ul
+                    className={`relative z-10 flex h-16 items-center justify-center gap-10 border-t border-white bg-slate-700 px-5 text-center text-base text-white`}
+                >
+                    <li
+                        className={`p-1 ${
+                            pathname === "/contact"
+                                ? "outline outline-1 outline-white"
+                                : ""
+                        }`}
+                    >
+                        <Link href="/contact">Contact Us</Link>
+                    </li>
+                    <li
+                        className={`p-1 ${
+                            pathname === "/legal/terms-of-service"
+                                ? "outline outline-1 outline-white"
+                                : ""
+                        }`}
+                    >
+                        <Link href="/legal/terms-of-service">
+                            Terms of Service
+                        </Link>
+                    </li>
+                    <li
+                        className={`p-1 ${
+                            pathname === "/legal/privacy-policy"
+                                ? "outline outline-1 outline-white"
+                                : ""
+                        }`}
+                    >
+                        <Link href="/legal/privacy-policy">Privacy Policy</Link>
+                    </li>
+                </ul>
+            </>
+        );
+
     return (
         <>
-            <ul className="relative z-10 flex h-16 items-center justify-center gap-10 border-t border-white bg-slate-700 px-5 text-white">
+            <ul
+                className={`relative z-10 flex h-16 items-center justify-center gap-10 border-t border-white bg-slate-700 px-5 text-center text-lg text-white`}
+            >
                 {/* <li className="absolute bottom-2 left-2 text-xs text-slate-300">
                     <Link className="group relative" href="/legal/attributions">
                         Attributions
