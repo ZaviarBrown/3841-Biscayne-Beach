ddfa5abe801a8d32604abe66f52ab8eb58c32aac
Zaviar Brown <brown.zaviar@gmail.com>
2024-02-26 21:16:38 -0700

    Adjusted styling for Mobile house-rules

--- src/pages/house-rules/index.tsx
+++ src/pages/house-rules/index.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import NavBarSpacer from "~/components/Home/NavBarSpacer";
+import { useMobileContext } from "~/context/MobileContext";
 
 export default function HouseRules() {
+    const { isMobile } = useMobileContext();
     const rules = [
         "No pets.",
         "No smoking on property.",
@@ -59,13 +61,13 @@ export default function HouseRules() {
             <NavBarSpacer />
 
             <div className="m-5 flex flex-col items-center gap-10 p-5">
-                <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-2xl text-slate-800 shadow-3xl">
-                    <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-3xl text-white">
+                <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-base text-slate-800 shadow-3xl md:text-2xl">
+                    <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-lg text-white md:text-3xl">
                         Please follow all Neighborhood Rules & please be
                         considerate of our neighbors right to peace and quiet.
                     </h1>
                     <div className="m-auto w-4/5 p-5">
-                        <ul className="grid grid-cols-2 gap-x-10">
+                        <ul className="grid grid-cols-1 gap-x-10 md:grid-cols-2">
                             {rules.map((rule, i) => (
                                 <li
                                     className="m-2 list-disc"
@@ -77,13 +79,13 @@ export default function HouseRules() {
                         </ul>
                     </div>
                 </div>
-                <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-2xl text-slate-800 shadow-3xl">
-                    <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-3xl text-white">
+                <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-base text-slate-800 shadow-3xl md:text-2xl">
+                    <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-lg text-white md:text-3xl">
                         Please follow check-in/out procedures and leave the
                         house like you found it.
                     </h1>
                     <div className="m-auto w-4/5 p-5">
-                        <ul className="grid grid-cols-2 gap-x-10">
+                        <ul className="grid grid-cols-1 gap-x-10 md:grid-cols-2">
                             {checkout.map((procedure, i) => (
                                 <li
                                     className="m-2 list-disc"
@@ -95,13 +97,13 @@ export default function HouseRules() {
                         </ul>
                     </div>
                 </div>
-                <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-2xl text-slate-800 shadow-3xl">
-                    <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-3xl text-white">
+                <div className="w-3/4 min-w-[350px] flex-grow rounded-lg bg-white text-base text-slate-800 shadow-3xl md:text-2xl">
+                    <h1 className="rounded-t-lg bg-slate-700 p-5 text-center text-lg text-white md:text-3xl">
                         Please note our Booking & Cancellation policies before
                         purchasing your booking.
                     </h1>
                     <div className="m-auto w-4/5 p-5">
-                        <ul className="grid grid-cols-2 gap-x-10">
+                        <ul className="grid grid-cols-1 gap-x-10 md:grid-cols-2">
                             {policies.map((policy, i) => (
                                 <li
                                     className="m-2 list-disc"
@@ -113,10 +115,21 @@ export default function HouseRules() {
                         </ul>
                     </div>
                 </div>
-                <iframe
-                    className="h-screen w-3/4"
-                    src="/assets/RentersRules.pdf"
-                />
+                {isMobile ? (
+                    <a
+                        className="mb-5 self-center rounded-lg border-2 border-slate-200 bg-slate-700 p-2 text-center text-lg text-white shadow-3xl"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://utfs.io/f/01dbbee1-2b07-4019-a0a1-91b9f68dae74-mgt2ly.pdf"
+                    >
+                        Tap here to view Renters Rules PDF
+                    </a>
+                ) : (
+                    <iframe
+                        className="h-screen w-3/4 self-center p-10"
+                        src="/assets/RentersRules.pdf"
+                    />
+                )}
             </div>
         </>
     );
