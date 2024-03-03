054c486e573e43095b7d83c4d2221e3cbe566ab0
Zaviar Brown <brown.zaviar@gmail.com>
2024-03-03 15:49:51 -0700

    Prevent Admin page from rendering in Mobile

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,12 +5,21 @@ import ManageBookings from "~/components/Admin/ManageBookings";
 import ManageUsers from "~/components/Admin/ManageUsers";
 import SideBar from "~/components/Admin/SideBar";
 import NavBarSpacer from "~/components/Home/NavBarSpacer";
+import { useMobileContext } from "~/context/MobileContext";
 
 export type PagesType = "bookings" | "pricing" | "users";
 
 export default function AdminPage() {
     const { data: session } = useSession();
+    const { isMobile } = useMobileContext();
     const [page, setPage] = useState<PagesType>("bookings");
+
+    if (isMobile)
+        return (
+            <h1 className="m-auto text-center text-3xl text-white">
+                This page cannot be rendered in a mobile view
+            </h1>
+        );
 
     if (session && session.user.role === "admin")
         return (
