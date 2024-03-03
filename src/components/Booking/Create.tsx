915aba5fa9a201df0ce09ff3bb7cdbaeb08e0716
Zaviar Brown <brown.zaviar@gmail.com>
2024-03-03 15:45:53 -0700

    Apply Calendar & PayPreview Mobile views

--- src/components/Booking/Create.tsx
+++ src/components/Booking/Create.tsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import type { DateRange } from "react-day-picker";
-import Calendar from "./Calendar";
-import PayPreview from "./PayPreview";
+import MobileCalendar from "./Mobile/Calendar";
+import DesktopCalendar from "./Desktop/Calendar";
+import MobilePayPreview from "./Mobile/PayPreview";
+import DesktopPayPreview from "./Desktop/PayPreview";
+import { useMobileContext } from "~/context/MobileContext";
 
 export default function CreateBooking() {
     const [dates, setDates] = useState<DateRange>({ from: undefined });
-    return (
-        <div className="m-5 flex justify-center gap-3 self-center rounded-lg bg-white p-10 shadow-3xl">
-            <Calendar dates={dates} setDates={setDates} />
-            <PayPreview selected={dates} />
-        </div>
-    );
+    const { isMobile } = useMobileContext();
+
+    if (isMobile)
+        return (
+            <div className="my-5 flex flex-col justify-center self-center rounded-lg bg-white shadow-3xl">
+                <MobileCalendar dates={dates} setDates={setDates} />
+                <MobilePayPreview selected={dates} />
+            </div>
+        );
+    else
+        return (
+            <div className="m-5 flex justify-center gap-3 self-center rounded-lg bg-white p-10 shadow-3xl">
+                <DesktopCalendar dates={dates} setDates={setDates} />
+                <DesktopPayPreview selected={dates} />
+            </div>
+        );
 }
