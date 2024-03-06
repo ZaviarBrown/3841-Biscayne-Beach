2d2cddeaf384b5acf6dae3494ce027cd7e53f7c9
Zaviar Brown <brown.zaviar@gmail.com>
2024-03-06 11:24:10 -0700

    Connect Google Ad account

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,26 +10,40 @@ import { ScrollContextProvider } from "~/context/ScrollContext";
 import { Modal, ModalContextProvider } from "~/context/ModalContext";
 import "default-passive-events";
 import { MobileContextProvider } from "~/context/MobileContext";
+import Script from "next/script";
 
 const MyApp: AppType<{ session: Session | null }> = ({
     Component,
     pageProps: { session, ...pageProps },
 }) => {
     return (
-        <SessionProvider session={session}>
-            <MobileContextProvider>
-                <ScrollContextProvider>
-                    <ModalContextProvider>
-                        <BookingContextProvider>
-                            <Modal />
-                            <Layout>
-                                <Component {...pageProps} />
-                            </Layout>
-                        </BookingContextProvider>
-                    </ModalContextProvider>
-                </ScrollContextProvider>
-            </MobileContextProvider>
-        </SessionProvider>
+        <>
+            <SessionProvider session={session}>
+                <MobileContextProvider>
+                    <ScrollContextProvider>
+                        <ModalContextProvider>
+                            <BookingContextProvider>
+                                <Modal />
+                                <Layout>
+                                    <Component {...pageProps} />
+                                </Layout>
+                            </BookingContextProvider>
+                        </ModalContextProvider>
+                    </ScrollContextProvider>
+                </MobileContextProvider>
+            </SessionProvider>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-EGSWZS95SK" />
+            <script
+                id="Google tag"
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-EGSWZS95SK');
+                        `,
+                }}
+            ></script>
+        </>
     );
 };
 
