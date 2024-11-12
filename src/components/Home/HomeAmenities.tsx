d7ef3b04d53cfd159ae33f25f469546407593cc7
Zaviar Brown <brown.zaviar@gmail.com>
2024-11-12 15:55:38 -0700

    Add cleanup TODO's

--- src/components/Home/HomeAmenities.tsx
+++ src/components/Home/HomeAmenities.tsx
@@ -1,21 +1,23 @@
-import ParallaxImage from "./ParallaxImage";
+import ParallaxImage from './ParallaxImage';
+
+// TODO: Use or delete this component
 
 export default function HomeAmenities() {
     return (
         <>
-            <ParallaxImage src="/landingPhotos/kitchen.jpg" alt="New Kitchen">
-                <div className="flex h-full w-full">
-                    <div className="w-1/3" />
-                    <div className="flex w-2/3 flex-col items-center justify-center gap-3 bg-black bg-opacity-70 text-center text-white">
-                        <h1 className="w-1/2 text-6xl">
+            <ParallaxImage src='/landingPhotos/kitchen.jpg' alt='New Kitchen'>
+                <div className='flex h-full w-full'>
+                    <div className='w-1/3' />
+                    <div className='flex w-2/3 flex-col items-center justify-center gap-3 bg-black bg-opacity-70 text-center text-white'>
+                        <h1 className='w-1/2 text-6xl'>
                             Equipped for your convenience
                         </h1>
-                        <p className="w-1/2 text-2xl">Full Kitchen</p>
-                        <p className="w-1/2 text-2xl">Washer / Dryer</p>
-                        <p className="w-1/2 text-2xl">Towels & Bed Linens</p>
-                        <p className="w-1/2 text-2xl">WiFi + Smart TV</p>
-                        <p className="w-1/2 text-2xl">Backup Generator</p>
-                        <p className="w-1/2 text-2xl">
+                        <p className='w-1/2 text-2xl'>Full Kitchen</p>
+                        <p className='w-1/2 text-2xl'>Washer / Dryer</p>
+                        <p className='w-1/2 text-2xl'>Towels & Bed Linens</p>
+                        <p className='w-1/2 text-2xl'>WiFi + Smart TV</p>
+                        <p className='w-1/2 text-2xl'>Backup Generator</p>
+                        <p className='w-1/2 text-2xl'>
                             Outdoor shower w/ hot water
                         </p>
                     </div>
