c2310195b782ef3f167b57abc8ebe883462adebf
Zaviar Brown <brown.zaviar@gmail.com>
2024-11-13 13:38:55 -0700

    Add new custom tailwind shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
-import { type Config } from "tailwindcss";
+import { type Config } from 'tailwindcss';
 
 export default {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             screens: {
-                book: "852px",
+                book: '852px',
             },
             boxShadow: {
-                "3xl": "0 6px 16px rgba(0,0,0,0.5)",
-                "4xlR": "10px 0px 16px rgba(0,0,0,0.5)",
-                "4xlL": "-10px 0px 16px rgba(0,0,0,0.5)",
+                '3xl': '0 6px 16px rgba(0,0,0,0.5)',
+                '3xl-white': '0 0 16px white',
+                '4xl': '0 0 16px black',
+                '4xlR': '10px 0px 16px rgba(0,0,0,0.5)',
+                '4xlL': '-10px 0px 16px rgba(0,0,0,0.5)',
             },
         },
     },
