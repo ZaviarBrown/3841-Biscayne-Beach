a282375daa69663e983a3e6e49329d9c554ac0a6
Zaviar Brown <brown.zaviar@gmail.com>
2024-11-14 17:50:40 -0700

    New tailwind shadow styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
             boxShadow: {
                 '3xl': '0 6px 16px rgba(0,0,0,0.5)',
                 '3xl-white': '0 0 16px white',
-                '4xl': '0 0 16px black',
+                '4xl': '0 0 20px 5px black',
                 '4xlR': '10px 0px 16px rgba(0,0,0,0.5)',
                 '4xlL': '-10px 0px 16px rgba(0,0,0,0.5)',
             },
