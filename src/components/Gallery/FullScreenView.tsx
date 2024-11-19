34dd7489b066cae95da7fc04ede3f397ff383382
Zaviar Brown <brown.zaviar@gmail.com>
2024-11-19 11:56:38 -0700

    Type import fix

--- src/components/Gallery/FullScreenView.tsx
+++ src/components/Gallery/FullScreenView.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import type { StaticImagesType } from "~/pages";
-import { useModalContext } from "~/context/ModalContext";
+import Image from 'next/image';
+import { useModalContext } from '~/context/ModalContext';
+import type { StaticImagesType } from '~/data';
 
 export default function FullScreenView({
     galleryImages,
@@ -21,43 +21,43 @@ export default function FullScreenView({
     };
 
     return (
-        <div className="flex h-[94vh] w-[96vw] items-center justify-between rounded-2xl border-2 border-slate-200 backdrop-blur-lg">
+        <div className='flex h-[94vh] w-[96vw] items-center justify-between rounded-2xl border-2 border-slate-200 backdrop-blur-lg'>
             {galleryImages.map(({ src, alt }, index) => (
                 <div
                     key={src}
                     className={`${
-                        index === galleryIndex ? "opacity-100" : "opacity-0"
+                        index === galleryIndex ? 'opacity-100' : 'opacity-0'
                     } fixed inset-0 m-auto h-[99%] w-[99%] transition-opacity duration-1000`}
                 >
                     <Image
                         src={src}
                         alt={alt}
-                        className="object-contain"
+                        className='object-contain'
                         fill
-                        sizes="99vw"
+                        sizes='99vw'
                     />
                 </div>
             ))}
 
             <button
                 onClick={closeModal}
-                className="absolute left-2 top-2 rounded-md border border-slate-200 bg-black px-4 py-2 text-white opacity-70 duration-200 hover:opacity-100"
+                className='absolute left-2 top-2 rounded-md border border-slate-200 bg-black px-4 py-2 text-white opacity-70 duration-200 hover:opacity-100'
             >
-                {"X"}
+                {'X'}
             </button>
 
             <button
                 onClick={previousImage}
-                className="mx-5 rounded-md border border-slate-200 bg-black px-4 py-2 text-white opacity-70 transition-opacity duration-200 hover:opacity-90"
+                className='mx-5 rounded-md border border-slate-200 bg-black px-4 py-2 text-white opacity-70 transition-opacity duration-200 hover:opacity-90'
             >
-                {"<"}
+                {'<'}
             </button>
 
             <button
                 onClick={nextImage}
-                className="mx-5 rounded-md border border-slate-200 bg-black px-4 py-2 text-white opacity-70 transition-opacity duration-200 hover:opacity-90"
+                className='mx-5 rounded-md border border-slate-200 bg-black px-4 py-2 text-white opacity-70 transition-opacity duration-200 hover:opacity-90'
             >
-                {">"}
+                {'>'}
             </button>
         </div>
     );
