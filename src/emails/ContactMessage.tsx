97f84174c84f0729e56bbaba2132787316446e19
ZaviarBrown <brown.zaviar@gmail.com>
2024-12-31 19:55:47 -0700

    Persist linebreaks in emails from customers

--- src/emails/ContactMessage.tsx
+++ src/emails/ContactMessage.tsx
@@ -31,25 +31,41 @@ export const ContactMessageEmail = ({
                     <Container className="my-auto max-w-[600px]">
                         <Section>
                             <Img
+                                key="Logo"
                                 src="https://utfs.io/f/ec4a6c83-e1e5-4be9-a579-867f895cabcf-1n7yu8.jpg"
                                 width="50"
                                 height="50"
                                 alt="3841 Biscayne Beach"
                                 className="inline-block rounded-full shadow-lg"
                             />{" "}
-                            <Text className="ml-1 inline-block h-full align-top text-xl font-semibold text-blue-500">
+                            <Text
+                                key="Self Ref"
+                                className="ml-1 inline-block h-full align-top text-xl font-semibold text-blue-500"
+                            >
                                 3841 Biscayne Beach
                             </Text>
                         </Section>
                         <Section>
                             <Row>
-                                <Text className="text-3xl font-bold">
+                                <Text
+                                    key="Intro"
+                                    className="text-3xl font-bold"
+                                >
                                     {`Here's what ${name} wrote`}
                                 </Text>
-                                <Text className="rounded-lg bg-slate-100 p-5 text-lg">
-                                    {message}
-                                </Text>
-                                <Text>
+                            </Row>
+                            <Row className="ml-2 rounded-lg bg-slate-100 px-5">
+                                {message.split("\n").map((line, key) => (
+                                    <Text
+                                        key={`Message Line #${key + 1}`}
+                                        className="text-lg"
+                                    >
+                                        {line}
+                                    </Text>
+                                ))}
+                            </Row>
+                            <Row>
+                                <Text key="Outro">
                                     You can respond by replying to this email.
                                 </Text>
                             </Row>
