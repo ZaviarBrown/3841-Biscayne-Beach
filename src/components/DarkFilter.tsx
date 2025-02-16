d9ce4c1e84d828d5bb0d1e9b5558f15186a1e8c9
Zaviar Brown <brown.zaviar@gmail.com>
2025-02-16 12:03:36 -0700

    Dark filter styling component

---
+++ src/components/DarkFilter.tsx
@@ -0,0 +1,8 @@
+export default function DarkFilter() {
+    return (
+        <>
+            <div className="absolute top-0 -z-40 h-1/2 w-full bg-gradient-to-b from-black to-transparent opacity-50" />
+            <div className="absolute bottom-0 -z-40 h-1/2 w-full bg-gradient-to-b from-transparent to-black opacity-50" />
+        </>
+    );
+}
